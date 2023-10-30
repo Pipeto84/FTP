09a68e7aaa1b043a614738447cd15d028331b0ba
Pipeto84 <pipejarauribeboteroa@gmail.com>
2023-10-29 21:21:09 -0500

    FTP with Syst and User

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,171 @@
-use std::net::TcpListener;
-use std::io::Write;
+use std::{net::{TcpListener,TcpStream},thread,io::{Write,Read,self}, path::PathBuf};
+#[derive(Debug, Clone, Copy)]
+#[repr(u32)]
+#[allow(dead_code)]
+enum ResultCode {
+    RestartMarkerReply = 110,
+    ServiceReadInXXXMinutes = 120,
+    DataConnectionAlreadyOpen = 125,
+    FileStatusOk = 150,
+    Ok = 200,
+    CommandNotImplementedSuperfluousAtThisSite = 202,
+    SystemStatus = 211,
+    DirectoryStatus = 212,
+    FileStatus = 213,
+    HelpMessage = 214,
+    SystemType = 215,
+    ServiceReadyForNewUser = 220,
+    ServiceClosingControlConnection = 221,
+    DataConnectionOpen = 225,
+    ClosingDataConnection = 226,
+    EnteringPassiveMode = 227,
+    UserLoggedIn = 230,
+    RequestedFileActionOkay = 250,
+    PATHNAMECreated = 257,
+    UserNameOkayNeedPassword = 331,
+    NeedAccountForLogin = 332,
+    RequestedFileActionPendingFurtherInformation = 350,
+    ServiceNotAvailable = 421,
+    CantOpenDataConnection = 425,
+    ConnectionClosed = 426,
+    FileBusy = 450,
+    LocalErrorInProcessing = 451,
+    InsufficientStorageSpace = 452,
+    UnknownCommand = 500,
+    InvalidParameterOrArgument = 501,
+    CommandNotImplemented = 502,
+    BadSequenceOfCommands = 503,
+    CommandNotImplementedForThatParameter = 504,
+    NotLoggedIn = 530,
+    NeedAccountForStoringFiles = 532,
+    FileNotFound = 550,
+    PageTypeUnknown = 551,
+    ExceededStorageAllocation = 552,
+    FileNameNotAllowed = 553,
+}
+#[derive(Debug,Clone)]
+enum Command {
+    Auth,
+    Syst,
+    Unknown(String),
+    User(String),
+}
+impl AsRef<str> for Command {
+    fn as_ref(&self) -> &str {
+        match *self {
+            Command::Auth => "AUTH",
+            Command::Syst => "SYST",
+            Command::Unknown(_) => "UNKN",
+            Command::User(_) => "USER",
+        }
+    }
+}
+impl Command {
+    pub fn new(input:Vec<u8>)->io::Result<Self> {
+        let mut iter=input.split(|&byte|byte == b' ');
+        let mut command=iter.next().expect("comando en entrada").to_vec();
+        to_uppercase(&mut command);
+        let data=iter.next();
+        let command=match command.as_slice() {
+            b"AUTH" => Command::Auth,
+            b"SYST" => Command::Syst,
+            b"USER" => Command::User(data.map(|bytes|String::from_utf8(bytes.to_vec())
+                .expect("no se pudo convertir bytes a string")).unwrap_or_default()),
+            s => Command::Unknown(std::str::from_utf8(s).unwrap_or("").to_owned()),
+        };
+        Ok(command)
+    }
+}
+fn to_uppercase(data:&mut [u8]) {
+    for byte in data {
+        if *byte >= 'a' as u8 && *byte <= 'z' as u8{
+            *byte -= 32;
+        }
+    }
+}
+#[allow(dead_code)]
+struct Client{
+    cwd: PathBuf,
+    stream: TcpStream,
+    name: Option<String>,
+}
+impl Client {
+    fn new(stream:TcpStream)->Client {
+        Client{
+            cwd:PathBuf::from("/"),
+            stream:stream,
+            name:None,
+        }
+    }
+    fn handle_cmd(&mut self,cmd:Command) {
+        println!("====> {:?}",cmd);
+        match cmd {
+            Command::Auth => send_cmd(&mut self.stream, ResultCode::CommandNotImplemented,
+                "no implementado"),
+            Command::Syst => send_cmd(&mut self.stream, ResultCode::Ok,
+                "no dire"),
+            Command::User(username) => {
+                if username.is_empty() {
+                    send_cmd(&mut self.stream, ResultCode::InvalidParameterOrArgument, 
+                        "nombre de usuario invalido");
+                }else {
+                    self.name=Some(username.to_owned());
+                    send_cmd(&mut self.stream, ResultCode::UserLoggedIn, 
+                    &format!("Bienvenido {}!",username))
+                }
+            }
+            Command::Unknown(s) => send_cmd(&mut self.stream, ResultCode::UnknownCommand, 
+                &format!("No se implemento: '{}'",s)),
+        }
+    }
+}
+fn read_all_message(stream:&mut TcpStream)->Vec<u8> {
+    let buf=&mut [0;1];
+    let mut out=Vec::with_capacity(100);
+    loop {
+        match stream.read(buf) {
+            Ok(received) if received > 0 =>{
+                if out.is_empty() && buf[0]==b' ' {
+                    continue
+                }
+                out.push(buf[0]);
+            }
+            _ => return Vec::new(),
+        }
+        let len=out.len();
+        if len > 1 && out[len -2]==b'\r' && out[len-1]==b'\n' {
+            out.pop();
+            out.pop();
+            return out;
+        }
+    }
+}
+fn send_cmd(stream:&mut TcpStream,code:ResultCode,message:&str) {
+    let msg=if message.is_empty() {
+        format!("{}\r\n",code as u32)
+    }else{
+        format!("{} {}\r\n",code as u32,message)
+    };
+    println!("<==== {}",msg);
+    write!(stream,"{}",msg).unwrap();
+}
+fn handle_client(mut stream:TcpStream) {
+    println!("Nuevo cliente conectado");
+    send_cmd(&mut stream, ResultCode::ServiceReadyForNewUser, "Bienvenido al servidor FTP");
+    let mut client=Client::new(stream);
+    loop {
+        let data=read_all_message(&mut client.stream);
+        if data.is_empty() {
+            println!("Cliente desconectado...");
+            break;
+        }
+        if let Ok(command) = Command::new(data) {
+            client.handle_cmd(command);
+        }else {
+            println!("Error con el comando de el cliente");
+        }
+    }
+}
 
 fn main() {
     let listener=TcpListener::bind("0.0.0.0:1234")
@@ -7,11 +173,10 @@ fn main() {
     println!("Esperando coneccion con clientes");
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                println!("Nuevo cliente");
-                if let Err(_) = stream.write(b"pipe") {
-                    println!("Fallo enviar pipe");
-                }
+            Ok(stream) => {
+                thread::spawn(move||{
+                    handle_client(stream);
+                });
             }
             _ => println!("Un cliente trato de conectarse")
         }
