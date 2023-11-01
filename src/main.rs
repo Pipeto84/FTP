dde903d702b8d5ffa74534aeb4a4a0d9ea76d8d2
Pipeto84 <pipejarauribeboteroa@gmail.com>
2023-11-01 09:52:56 -0500

    FTP server basic

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{net::{TcpListener,TcpStream},thread,io::{Write,Read,self}, path::PathBuf};
+use std::{net::{TcpListener,TcpStream,IpAddr,Ipv4Addr,SocketAddr},thread,io::{Write,Read,self},path::PathBuf};
 #[derive(Debug, Clone, Copy)]
 #[repr(u32)]
 #[allow(dead_code)]
@@ -47,16 +47,24 @@ enum ResultCode {
 enum Command {
     Auth,
     Syst,
-    Unknown(String),
+    Pwd,
+    NoOp,
+    Type,
+    Pasv,
     User(String),
+    Unknown(String),
 }
 impl AsRef<str> for Command {
     fn as_ref(&self) -> &str {
         match *self {
             Command::Auth => "AUTH",
             Command::Syst => "SYST",
-            Command::Unknown(_) => "UNKN",
+            Command::Pwd => "PWD",
+            Command::NoOp => "NOOP",
+            Command::Type => "TYPE",
+            Command::Pasv => "PASV",
             Command::User(_) => "USER",
+            Command::Unknown(_) => "UNKN",
         }
     }
 }
@@ -69,6 +77,10 @@ impl Command {
         let command=match command.as_slice() {
             b"AUTH" => Command::Auth,
             b"SYST" => Command::Syst,
+            b"PWD" => Command::Pwd,
+            b"NOOP" => Command::NoOp,
+            b"TYPE" => Command::Type,
+            b"PASV" => Command::Pasv,
             b"USER" => Command::User(data.map(|bytes|String::from_utf8(bytes.to_vec())
                 .expect("no se pudo convertir bytes a string")).unwrap_or_default()),
             s => Command::Unknown(std::str::from_utf8(s).unwrap_or("").to_owned()),
@@ -88,6 +100,7 @@ struct Client{
     cwd: PathBuf,
     stream: TcpStream,
     name: Option<String>,
+    data_writer: Option<TcpStream>,
 }
 impl Client {
     fn new(stream:TcpStream)->Client {
@@ -95,6 +108,7 @@ impl Client {
             cwd:PathBuf::from("/"),
             stream:stream,
             name:None,
+            data_writer:None,
         }
     }
     fn handle_cmd(&mut self,cmd:Command) {
@@ -104,6 +118,37 @@ impl Client {
                 "no implementado"),
             Command::Syst => send_cmd(&mut self.stream, ResultCode::Ok,
                 "no dire"),
+            Command::Pwd => {
+                let msg=format!("{}",self.cwd.to_str().unwrap_or(""));
+                if !msg.is_empty() {
+                    let message=format!("\"/{}\" ",msg);
+                    send_cmd(&mut self.stream, ResultCode::PATHNAMECreated,
+                        &format!("\"/{}\" ",msg))
+                }else {
+                    send_cmd(&mut self.stream, ResultCode::FileNotFound, 
+                        "No existe archivo o directorio")
+                }
+            }
+            Command::NoOp => send_cmd(&mut self.stream, ResultCode::Ok,"Haciendo nada"),
+            Command::Type => send_cmd(&mut self.stream, ResultCode::Ok,
+                "Transferir exitosamente cambio de type"),
+            Command::Pasv => {
+                if  self.data_writer.is_some() {
+                    send_cmd(&mut self.stream,ResultCode::DataConnectionAlreadyOpen,
+                        "Ya eschuchando")
+                }else {
+                    let port: u16=43210;
+                    send_cmd(&mut self.stream,ResultCode::EnteringPassiveMode,
+                        &format!("127,0,0,1,{},{}",port>>8,port & 0xFF));
+                    let addr=SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127,0,0,1)),port);
+                    let listener=TcpListener::bind(&addr).unwrap();
+                    match listener.incoming().next() {
+                        Some(Ok(client)) =>{self.data_writer=Some(client);}
+                        _ => {send_cmd(&mut self.stream,ResultCode::ServiceNotAvailable,
+                            "Los problemas suceden");}
+                    }
+                }
+            }
             Command::User(username) => {
                 if username.is_empty() {
                     send_cmd(&mut self.stream, ResultCode::InvalidParameterOrArgument, 
@@ -111,7 +156,7 @@ impl Client {
                 }else {
                     self.name=Some(username.to_owned());
                     send_cmd(&mut self.stream, ResultCode::UserLoggedIn, 
-                    &format!("Bienvenido {}!",username))
+                        &format!("Bienvenido {}!",username));
                 }
             }
             Command::Unknown(s) => send_cmd(&mut self.stream, ResultCode::UnknownCommand, 
@@ -153,8 +198,12 @@ fn handle_client(mut stream:TcpStream) {
     println!("Nuevo cliente conectado");
     send_cmd(&mut stream, ResultCode::ServiceReadyForNewUser, "Bienvenido al servidor FTP");
     let mut client=Client::new(stream);
+    let mut loops=0;
     loop {
+        loops +=1;
+        println!("->{:?}",loops);
         let data=read_all_message(&mut client.stream);
+        println!("data:{:?}",String::from_utf8(data.clone()));
         if data.is_empty() {
             println!("Cliente desconectado...");
             break;
@@ -164,9 +213,23 @@ fn handle_client(mut stream:TcpStream) {
         }else {
             println!("Error con el comando de el cliente");
         }
+        println!("<-{:?}",loops);
     }
 }
-
+fn handle(mut stream:TcpStream) {
+    println!("Nuevo cliente pipe");
+    send_cmd(&mut stream, ResultCode::ServiceReadyForNewUser, "Bienvenido al servidor FTP");
+    let mut client=Client::new(stream);
+    for _ in 0..6 {
+        let data=read_all_message(&mut client.stream);
+        // println!("{} {:?}",line!(),String::from_utf8(data.clone()));
+        if let Ok(command) = Command::new(data) {
+            client.handle_cmd(command);
+        }else {
+            println!("Error con el comando de el cliente");
+        }        
+    }
+}
 fn main() {
     let listener=TcpListener::bind("0.0.0.0:1234")
         .expect("fallo enlace a la direccion");
@@ -175,7 +238,7 @@ fn main() {
         match stream {
             Ok(stream) => {
                 thread::spawn(move||{
-                    handle_client(stream);
+                    handle(stream);
                 });
             }
             _ => println!("Un cliente trato de conectarse")
